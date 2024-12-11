09f438f8425c6f74f26194fcbdf6743101c7089e
Gaussdrigo <alvarengar49@gmail.com>
2024-12-11 18:25:19 -0300

    Primer commit: Configuración inicial del proyecto Spring Boot

---
+++ src/main/java/com/example/demo/ApificarApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApificarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApificarApplication.class, args);
+	}
+
+}
